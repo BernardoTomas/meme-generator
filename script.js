8960d63a4644225d4a15c3b8ebe2695c64fec12a
Bernardo Tomas <bernardotomas.c14@gmail.com>
2024-02-12 15:25:22 -0300

    <FEAT> Requisito 2 feito

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const memeInsert = document.getElementById('meme-insert');
+
+const loadMemePreview = () => {
+  memeInsert.addEventListener('change', (event) => {
+    const memeImage = document.getElementById('meme-image');
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+    memeImage.onload = () => {
+      URL.revokeObjectURL(memeImage.src);
+    };
+  });
+};
 
 const getMemeText = () => {
   textInput.addEventListener('keyup', () => {
@@ -8,3 +19,4 @@ const getMemeText = () => {
 };
 
 getMemeText();
+loadMemePreview();
