17e9f537e0a46bba352c638a0ac465fb3f52bf85
Bernardo Tomas <bernardotomas.c14@gmail.com>
2024-02-13 08:09:25 -0300

    <FEAT> Fiz os requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
 const memeInsert = document.getElementById('meme-insert');
+const memeContainer = document.getElementById('meme-image-container');
+const fireBtn = document.getElementById('fire');
+const waterBtn = document.getElementById('water');
+const earthBtn = document.getElementById('earth');
+
+const firePreviewBorder = () => {
+  fireBtn.addEventListener('click', () => {
+    memeContainer.style.border = '3px dashed rgb(255 , 0 , 0)';
+  });
+};
+
+const waterPreviewBorder = () => {
+  waterBtn.addEventListener('click', () => {
+    memeContainer.style.border = '5px double rgb(0 , 0 , 255)';
+  });
+};
+
+const earthPreviewBorder = () => {
+  earthBtn.addEventListener('click', () => {
+    memeContainer.style.border = '6px groove rgb(0 , 128 , 0)';
+  });
+};
 
 const loadMemePreview = () => {
   memeInsert.addEventListener('change', (event) => {
@@ -14,9 +36,12 @@ const loadMemePreview = () => {
 
 const getMemeText = () => {
   textInput.addEventListener('keyup', () => {
-    memeText.innerHTML = `<h2>${textInput.value}</h2>`;
+    memeText.innerText = textInput.value;
   });
 };
 
 getMemeText();
 loadMemePreview();
+firePreviewBorder();
+waterPreviewBorder();
+earthPreviewBorder();
